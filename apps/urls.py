130182bb3accee4a9d0da7a3c2b76f65d95f70c8
xmll7 <karshiboyevfarrux@gmail.com>
2025-06-16 18:22:19 +0500

    sqlite

--- apps/urls.py
+++ apps/urls.py
@@ -3,4 +3,5 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns=[
     # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
--- apps/views.py
+++ apps/views.py
@@ -1,5 +1,2 @@
 from django.http import JsonResponse
 
-
-def hello_world(request):
-    return JsonResponse({'message': 'hello world'})
